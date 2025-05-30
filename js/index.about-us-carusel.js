0157a1ba6e6fa5e8a6b8f6161cf30f77cbed2963
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2025-05-30 14:16:27 +0000

    fix embla

--- js/index.about-us-carusel.js
+++ js/index.about-us-carusel.js
@@ -1,20 +1,16 @@
-import EmblaCarousel from "embla-carousel";
+const emblaNode = document.querySelector('.embla');
+const viewportNode = emblaNode.querySelector('.embla__viewport');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const emblaNode = document.querySelector('.embla');
-  const viewportNode = emblaNode.querySelector('.embla__viewport');
-
-  const embla = EmblaCarousel(viewportNode, {
-    loop: true,
-    align: 'start',
-    slidesToScroll: 1,
-    speed: 10,
-  });
-
-  const btnPrev = emblaNode.querySelector('.embla__prev');
-  const btnNext = emblaNode.querySelector('.embla__next');
-
-  btnPrev.addEventListener('click', embla.scrollPrev);
-  btnNext.addEventListener('click', embla.scrollNext);
+const embla = EmblaCarousel(viewportNode, {
+  loop: true,
+  align: 'start',
+  slidesToScroll: 1,
+  speed: 10,
 });
+
+const btnPrev = emblaNode.querySelector('.embla__prev');
+const btnNext = emblaNode.querySelector('.embla__next');
+
+btnPrev.addEventListener('click', embla.scrollPrev);
+btnNext.addEventListener('click', embla.scrollNext);
 
